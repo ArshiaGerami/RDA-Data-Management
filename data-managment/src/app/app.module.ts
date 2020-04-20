ecac03524ac57904ae3d29f0cd4ee631d775afe8
farzinGhaheri <f.ghaheri@gmail.com>
2020-04-20 20:30:10 +0800

    Add nav-bar

--- data-managment/src/app/app.module.ts
+++ data-managment/src/app/app.module.ts
@@ -1,39 +1,42 @@
 import { BrowserModule, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
-// import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
+
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-// import { MatIconRegistry, MatSelectModule } from "@angular/material";
-// import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconRegistry } from '@angular/material/icon';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { DatePipe } from '@angular/common';
+import { NavBarComponent } from './common/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule,
-    MatCardModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
     MatButtonToggleModule,
-   // FormsModule,
-    // ReactiveFormsModule,
-    // HttpClientModule,
+    MatCardModule,
+    HttpClientModule,
     FontAwesomeModule,
   ],
-  providers: [],
+  providers: [DatePipe, MatIconRegistry],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  // constructor(public matIconRegistry: MatIconRegistry) {
-  //   matIconRegistry.registerFontClassAlias('fontawesome', 'fa');
-  // }
+  constructor(public matIconRegistry: MatIconRegistry) {
+    matIconRegistry.registerFontClassAlias('fontawesome', 'fa');
+  }
 }
