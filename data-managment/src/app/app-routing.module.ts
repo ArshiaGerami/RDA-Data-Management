14d78b6d00c1ffd8eccdc5b4dd1818cd63b59945
farzinGhaheri <f.ghaheri@gmail.com>
2020-04-21 01:21:04 +0800

    Add signUp Component

--- data-managment/src/app/app-routing.module.ts
+++ data-managment/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignUpComponent } from './common/sign-up/sign-up.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'en/sign-up',
+    component: SignUpComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- data-managment/src/app/app.module.ts
+++ data-managment/src/app/app.module.ts
@@ -7,18 +7,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MatSelectModule } from '@angular/material/select';
-import { MatIconRegistry } from '@angular/material/icon';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { FormsModule, ReactiveFormsModule,  } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { DatePipe } from '@angular/common';
 import { NavBarComponent } from './common/nav-bar/nav-bar.component';
+import { SignUpComponent } from './common/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
@@ -31,6 +35,9 @@ import { NavBarComponent } from './common/nav-bar/nav-bar.component';
     MatCardModule,
     HttpClientModule,
     FontAwesomeModule,
+    FlexLayoutModule,
+    MatInputModule,
+    MatIconModule,
   ],
   providers: [DatePipe, MatIconRegistry],
   bootstrap: [AppComponent]
